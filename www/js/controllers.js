5ac9de70a2069a15787b78084506ed1c3c391f50
adelaineho <adelaineho@hipagesgroup.com.au>
2015-03-06 12:16:30 +1100

    Oracle Phase 1

---
+++ www/js/controllers.js
@@ -0,0 +1,184 @@
+angular.module('oracle.controllers', [])
+    .directive('fileModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        model.assign(scope, element[0].files[0]);
+                    });
+                    this.classList.add('hide');
+                    scope.$parent.buttonStatus = "Get started";
+                    scope.$parent.selectedImageName = element[0].files[0].name;
+                    scope.$apply();
+                });
+            }
+        };
+    }])
+
+    .controller('OracleCtrl', function($scope, $stateParams, classifiedService) {
+        $scope.categories = null;
+        $scope.mostRelevant = '';
+        $scope.navTitle = '<img class="logo" src="http://assets.homeimprovementpages.com.au/css/images/hip/hip_logo.svg" />';
+        $scope.classify = function(str) {
+            classifiedService.fetchClassified(str).then(function(response) {
+                var result = [];
+                var jsonObj;
+                var predictions = response.data.predictions;
+                for(i=0;i<predictions.length;i++){
+                    result.push(predictions[i].class_name);
+                }
+                var firstCategory = result.shift();
+                var catKeys = result;
+                classifiedService.getJson().then(function(response){
+                    jsonObj = response.data;
+                    $scope.mostRelevant = jsonObj[firstCategory];
+                    var catObj = {};
+                    for(j=0;j<catKeys.length;j++){
+                        if(typeof jsonObj[catKeys[j]] !== 'undefined'){
+                            catObj[catKeys[j]] = jsonObj[catKeys[j]];
+                        }
+                    }
+                    $scope.categories = catObj;
+                });
+            });
+        };
+
+        $scope.showSuggestedCategories = function() {
+            $scope.clicked = true;
+            $scope.mostRelevant = '';
+        };
+
+        $scope.hideSuggestedCategories = function() {
+            $scope.clicked = false;
+        };
+
+        $scope.callLink = function(catKey) {
+            window.open('http://www.homeimprovementpages.com.au/find/'+catKey+'/get_quotes_mobile', '_system', 'location=yes'); return false;
+        };
+    })
+
+    .controller('CameraCtrl', function($scope, $cordovaCapture) {
+        document.addEventListener("deviceready", function () {
+            $scope.captureAudio = function() {
+                var options = { limit: 3, duration: 10 };
+
+                $cordovaCapture.captureAudio(options).then(function(audioData) {
+                    // Success! Audio data is here
+                }, function(err) {
+                    // An error occurred. Show a message to the user
+                });
+            };
+
+            $scope.captureImage = function() {
+                var options = { limit: 3 };
+
+                $cordovaCapture.captureImage(options).then(function(imageData) {
+                    // Success! Image data is here
+                }, function(err) {
+                    // An error occurred. Show a message to the user
+                });
+            };
+
+        }, false);
+    })
+
+    .controller('UploadCtrl', function($scope, $stateParams, $cordovaCamera, Camera, cloudSight, $timeout, classifiedService) {
+        document.addEventListener("deviceready", function () {
+            var options = {
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.CAMERA
+            };
+            $cordovaCamera.getPicture(options).then(function(imageURI) {
+                var image = document.getElementById('myImage');
+                image.src = imageURI;
+            }, function(err) {
+                // error
+            });
+            $scope.getPhoto = function() {
+                Camera.getPicture().then(function(imageURI) {
+                    console.log(imageURI);
+                }, function(err) {
+                    console.err(err);
+                });
+            };
+            $cordovaCamera.cleanup().then(); // only for FILE_URI
+        }, false);
+
+        $scope.buttonStatus = 'Upload';
+        $scope.processing = false;
+        $scope.sendToCloudSight = function(file){
+            if(typeof file !== 'undefined'){
+                cloudSight.uploadFileToUrl(file)
+                    .then(function(response){
+                        var meta = response.data;
+                        $scope.imageReturned =  meta.url;
+                        $scope.selectedImageName = '';
+                        $scope.progressButtonStatus = 'Interpreting...';
+                        var poll = function() {
+                            $timeout(function() {
+                                cloudSight.getImageData(meta.token).then(function(response){
+                                    if(response.data.status == 'completed') {
+                                        classifiedService.fetchClassified(response.data.name).then(function(response) {
+                                            var result = [];
+                                            var jsonObj;
+                                            var predictions = response.data.predictions;
+                                            for(i=0;i<predictions.length;i++){
+                                                result.push(predictions[i].class_name);
+                                            }
+                                            var firstCategory = result.shift();
+                                            var catKeys = result;
+                                            classifiedService.getJson().then(function(response){
+                                                jsonObj = response.data;
+                                                $scope.mostRelevant = jsonObj[firstCategory];
+                                                var catObj = {};
+                                                for(j=0;j<catKeys.length;j++){
+                                                    if(typeof jsonObj[catKeys[j]] !== 'undefined'){
+                                                        catObj[catKeys[j]] = jsonObj[catKeys[j]];
+                                                    }
+                                                }
+                                                $scope.categories = catObj;
+                                                $scope.processing = false;
+                                            });
+                                        });
+                                    } else {
+                                        poll();
+                                    }
+                                });
+                            }, 1000);
+                        };
+                        poll();
+                    });
+                    $scope.processing = true;
+                    $scope.progressButtonStatus = 'Uploading';
+            }
+        };
+
+        $scope.showSuggestedCategories = function() {
+            $scope.clicked = true;
+            $scope.mostRelevant = '';
+        };
+
+        $scope.hideSuggestedCategories = function() {
+            $scope.clicked = false;
+        };
+
+        $scope.refreshUpload = function() {
+            $state.go('transition', {destination: 'tab.upload'});
+        };
+
+        $scope.goToOracle = function() {
+            $state.go('transition', {destination: 'tab.oracle'});
+        };
+
+        $scope.callLink = function(catKey) {
+            window.open('http://www.homeimprovementpages.com.au/find/'+catKey+'/get_quotes_mobile', '_system', 'location=yes'); return false;
+        };
+    })
+
+    .controller('ContentController', function($scope, $ionicSideMenuDelegate) {
+        $scope.toggleRight = function() {
+            $ionicSideMenuDelegate.toggleRight();
+        };
+    });
---
+++ www/js/services.js
@@ -0,0 +1,187 @@
+angular.module('oracle.services', [])
+    .service('classifiedService', function($http) {
+        return {
+            fetchClassified: function(str) {
+                var dataObj = {
+                    classifier_id : 6619,
+                    value : str
+                };
+                var req = {
+                    method: 'POST',
+                    url: 'https://www.metamind.io/language/classify',
+                    headers: {
+                        'Authorization': "Basic nytPC420X70Al0iL2EUgAerhPKty24vsjMiCJCvwV2NQBHK0eN",
+                        'Content-Type': "application/json"
+                    },
+                    data: dataObj
+                };
+                return $http(req)
+                    .error(function(data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        console.warn('can\'t classify');
+                    });
+            },
+            getJson: function() {
+                return $http.get("/js/category.json")
+                    .error(function(data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        console.warn('no data returned from json');
+                    });
+            }
+        };
+    })
+
+    .service('cloudSight', ['$http', function ($http) {
+        this.uploadFileToUrl = function(file){
+            var fd = new FormData();
+            fd.append('file', file);
+            fd.append('image_request[image]',file);
+            fd.append('image_request[locale]','en-US');
+            var req = {
+                method: 'POST',
+                url: 'https://api.cloudsightapi.com/image_requests',
+                transformRequest: angular.identity,
+                headers: {
+                    'Authorization': "CloudSight uNlREkBTDU0eIXBeuyxUTg",
+                    'Content-Type': undefined
+                },
+                data: fd
+            };
+            return $http(req)
+                .error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.warn('something went wrong when sending to cloudsight');
+                });
+        };
+        this.getImageData = function(token) {
+            var req = {
+                method: 'GET',
+                url: 'https://api.cloudsightapi.com/image_responses/'+token,
+                headers: {
+                    'Authorization': "CloudSight uNlREkBTDU0eIXBeuyxUTg",
+                    'Content-Type': 'application/json'
+                }
+            };
+            return $http(req)
+                .error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    console.warn('something went wrong when sending to cloudsight');
+                });
+        }
+    }])
+
+    .factory('Camera', ['$q', function($q) {
+        return {
+            getPicture: function(options) {
+                var q = $q.defer();
+
+                navigator.camera.getPicture(function(result) {
+                    // Do any magic you need
+                    q.resolve(result);
+                }, function(err) {
+                    q.reject(err);
+                }, options);
+
+                return q.promise;
+            }
+        }
+    }])
+
+    .factory('Chats', function() {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+      }, {
+        id: 2,
+        name: 'Andrew Jostlin',
+        lastText: 'Did you get the ice cream?',
+        face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+      }, {
+        id: 3,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+      }, {
+        id: 4,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+      }];
+
+      return {
+        all: function() {
+          return chats;
+        },
+        remove: function(chat) {
+          chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+          for (var i = 0; i < chats.length; i++) {
+            if (chats[i].id === parseInt(chatId)) {
+              return chats[i];
+            }
+          }
+          return null;
+        }
+      }
+    })
+
+    /**
+     * A simple example service that returns some data.
+     */
+    .factory('Friends', function() {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var friends = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        notes: 'Enjoys drawing things',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      }, {
+        id: 1,
+        name: 'Max Lynx',
+        notes: 'Odd obsession with everything',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+      }, {
+        id: 2,
+        name: 'Andrew Jostlen',
+        notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
+        face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+      }, {
+        id: 3,
+        name: 'Adam Bradleyson',
+        notes: 'I think he needs to buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+      }, {
+        id: 4,
+        name: 'Perry Governor',
+        notes: 'Just the nicest guy',
+        face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+      }];
+
+
+      return {
+        all: function() {
+          return friends;
+        },
+        get: function(friendId) {
+          // Simple index lookup
+          return friends[friendId];
+        }
+      }
+    });
