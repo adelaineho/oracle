6ad7ec9cdb54db22d9a1158996e5bddd1e228aff
adelaineho <adelaineho@hipagesgroup.com.au>
2016-02-10 16:54:34 +1100

    Change to relative path and update copy on Upload screen

--- www/js/services.js
+++ www/js/services.js
@@ -8,7 +8,7 @@ angular.module('oracle.services', [])
                 };
                 var req = {
                     method: 'POST',
-                    url: 'http://oracle.hipages.com.au:8080/classify',
+                    url: '/classify',
                     withCredentials: true,
                     headers: {
                         'Authorization': "Basic 7zZmxxJNVhLH1ZeuCxu3UHwDxCAE8fQFXWyIzejwnNe9vxLLLC",
